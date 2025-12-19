5b5dd3638d9a80d6cff3a9113f30c272a74c5135
stanqb <9857@holbertonstudents.com>
2025-12-19 11:26:15 +0100

    Task 11: change stack order using setTimeout for prime calculation

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+    let count = 0;
+  
+    for (let num = 2; num <= 100; num++) {
+      let isPrime = true;
+  
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+  
+      if (isPrime) {
+        count++;
+      }
+    }
+  
+    return count;
+  }
+  
+  const startTime = performance.now();
+  
+  setTimeout(function() {
+    for (let i = 0; i < 100; i++) {
+      countPrimeNumbers();
+    }
+  
+    const endTime = performance.now();
+    console.log('Execution time of calculating prime numbers 100 times was ' + (endTime - startTime) + ' milliseconds.');
+  }, 0);
